bff4f470028107a3eede34fb066927df20c2adde
Marco Correa <35812048+MCorrea675@users.noreply.github.com>
2018-05-17 15:19:46 -0700

    Update HashEntry.h

--- HashEntry.h
+++ HashEntry.h
@@ -2,10 +2,10 @@ class HashEntry {
 public:
 	int key;
 	int value;
-	HashNode* next;
+	//HashNode* next;
 	HashEntry(int key, int value) {
 		this->key = key;
 		this->value = value;
-		this->next = NULL;
+		//this->next = NULL;
 	}
 };
