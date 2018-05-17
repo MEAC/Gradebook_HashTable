b566bbd2ddc72b88edd63613b3db3b521ff1d29e
Marco Correa <35812048+MCorrea675@users.noreply.github.com>
2018-05-17 10:10:03 -0700

    Update Password_Hash-Table.cpp

--- Password_Hash-Table.cpp
+++ Password_Hash-Table.cpp
@@ -2,7 +2,15 @@
 #include <iostream>
 
 
-int main()
-{
+int main(int argc, char * argv[]) {
+	ofstream outputFile;
+	string input;
+	string fileName;
+
+	cout << "Enter file name: ";
+	cin >> fileName;
+
+	outputFile.open(fileName.c_str());
+    
     return 0;
 }
